db184e32501d10c2054cb3d06e07652df3c2616a
szone2s@naver.com <devmon>
2026-04-12 18:36:35 +0900

    fix

--- ProjectA/Assets/Sendbox/View/Scripts/SystemPopupView.cs
+++ ProjectA/Assets/Sendbox/View/Scripts/SystemPopupView.cs
@@ -2,15 +2,38 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class SystemPopupView : MonoBehaviour, ISystemPopupView
 {
     [SerializeField] private TMP_Text titleText;
     [SerializeField] private TMP_Text messageText;
     [SerializeField] private Button confirmButton;
+    [SerializeField] private float animDuration = 0.4f;
+    [SerializeField] private float slideDistance = 300f;
 
-    public void Display() => gameObject.SetActive(true);
-    public void Hide() => gameObject.SetActive(false);
+    private RectTransform rectTransform;
+    private Vector2 originPosition;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originPosition = rectTransform.anchoredPosition;
+    }
+
+    public void Display()
+    {
+        gameObject.SetActive(true);
+        rectTransform.anchoredPosition = originPosition + Vector2.up * slideDistance;
+        rectTransform.DOAnchorPos(originPosition, animDuration).SetEase(Ease.OutBack);
+    }
+
+    public void Hide()
+    {
+        rectTransform.DOAnchorPos(originPosition - Vector2.up * slideDistance, animDuration)
+            .SetEase(Ease.InBack)
+            .OnComplete(() => gameObject.SetActive(false));
+    }
 
     public void Initialize(string title, string message, Action onConfirm = null)
     {
@@ -25,6 +48,7 @@ public class SystemPopupView : MonoBehaviour, ISystemPopupView
 
     public void Dispose()
     {
+        rectTransform.DOKill();
         Destroy(gameObject);
     }
 }
